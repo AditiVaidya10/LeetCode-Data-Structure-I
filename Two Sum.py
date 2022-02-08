dbbd4afe719210421e61a1f92c79ca86ec5a0e9b
Aditi Atul Vaidya <61463717+AditiVaidya10@users.noreply.github.com>
2022-02-08 11:25:29 +0530

    Create Two Sum.py

---
+++ Two Sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums, target):
+        h = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            if n not in h:
+                h[num] = i
+            else:
+                return [h[n], i]
